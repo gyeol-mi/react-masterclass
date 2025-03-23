e616e2cebf6750f70bcf1dfeedaea08f4b4e5804
emily kang [강경임] <strongwomankky@gmail.com>
2025-03-23 19:50:00 +0900

    #2.2_Adapting and Extending

--- src/App.js
+++ src/App.js
@@ -4,29 +4,20 @@ const Father = styled.div`
   display: flex;
 `;
 
-const BoxOne = styled.div`
-  background-color: tomato;
+const Box = styled.div`
+  background-color: ${(props) => props.bgColor};
   width: 100px;
   height: 100px;
 `;
 
-const BoxTwo = styled.div`
-  background-color: teal;
-  width: 100px;
-  height: 100px;
+const Circle = styled(Box)`
+  border-radius: 50px;
 `;
-
-const Text = styled.h1`
-  color: white;
-`;
-
 function App() {
   return (
     <Father>
-      <BoxOne>
-        <Text>Hello</Text>
-      </BoxOne>
-      <BoxTwo />
+      <Box bgColor="teal" />
+      <Circle bgColor="tomato" />
     </Father>
   );
 }
