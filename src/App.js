ddca5c0307422872977dfade68ab8489171949da
emily kang [강경임] <strongwomankky@gmail.com>
2025-03-23 21:15:43 +0900

    #2.5.1_Pseudo Selectors part One

--- src/App.js
+++ src/App.js
@@ -20,15 +20,32 @@ const rotation = keyframes`
 `;
 
 const Box = styled.div`
+  display: flex;
+  justify-content: center;
+  align-items: center;
   height: 200px;
   width: 200px;
   background-color: tomato;
   animation: ${rotation} 5s linear infinite;
+  //pseudo-selector
+  //styled-component가 아닌 요소를 부모 styled-component요소에서 선택할 수 있음
+  span {
+    font-size: 36px;
+    //span:hover{}와 같음-단축키처럼 쓴 코드드
+    &:hover {
+      font-size: 50px;
+    }
+    &:active {
+      opacity: 0;
+    }
+  }
 `;
 function App() {
   return (
     <Wrapper>
-      <Box />
+      <Box>
+        <span>😊</span>
+      </Box>
     </Wrapper>
   );
 }
